05bd3685904211e43e6f0ee1fab9561f03b705f7
Antipire <gronos02@gmail.com>
2024-12-09 15:56:07 +0300

    Flask client now shows directory content in json format

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask
+import configparser
+from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
+# Чтение конфигурации
+config = configparser.ConfigParser()
+config.read('config.ini')
+directory_to_watch = config['Directories']['directory_to_watch']
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
+def get_directory_contents():
+    contents = os.listdir(directory_to_watch)
+    return jsonify(contents)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
